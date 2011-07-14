f62bc5d5651aaf8260c819d9c0d4aeb72d1c664f
Nicolas Favre-Felix <n.favrefelix@gmail.com>
2011-07-14 18:56:34 +0100

    Added more ZCOUNT tests

--- test-zset.sh
+++ test-zset.sh
@@ -2,16 +2,18 @@
 
 redis="../redis/src/redis-cli -p 1111"
 
-$redis ZADD x 0 a
-$redis ZADD x 1 b
-$redis ZADD x 2 c
-$redis ZADD x 3 d
-$redis ZADD x 4 e
-$redis ZADD x 5 f
-$redis ZADD x 6 g
-$redis ZADD x 7 h
-$redis ZADD x 8 i
-$redis ZADD x 9 j
+$redis ZADD x 1 a
+$redis ZADD x 2 b
+$redis ZADD x 3 c
+$redis ZADD x 4 d
+$redis ZADD x 5 e
+$redis ZADD x 6 f
+$redis ZADD x 7 g
+$redis ZADD x 8 h
+$redis ZADD x 9 i
+$redis ZADD x 10 j
 
-$redis ZCOUNT x 3 7
-$redis ZCOUNT x 20 20
+$redis ZCOUNT x 3 7	# expecting 5
+$redis ZCOUNT x 20 20	# expecting 0
+$redis ZCOUNT x 0 0	# expecting 0
+$redis ZCOUNT x 1 10	# expecting 10
